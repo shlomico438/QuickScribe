6c045ed2fc301db40ada063f7928fa0fe6f53cf3
shlomico438 <shlomico1234@gmail.com>
2026-01-18 14:13:26 +0200

    architecture change moving to chunk

--- handler.py
+++
@@ -1,4 +0,0 @@
-def handler(event):
-    return {
-        "output": "RunPod serverless is working"
-    } 
--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
-docker run --rm -v "C:\Work\runpod\QuickScribe\Audio:/data" -v "C:\Work\runpod\QuickScribe\Audio:/input/output" whisperx-container
+@echo off
+docker run --rm ^
+  --env-file .env ^
+  -v "C:\Work\runpod\QuickScribe\Audio:/data" ^
+  -v "C:\Work\runpod\QuickScribe\Audio:/tmp/QuickScribeOutput" ^
+  whisperx-container
